4482f6b03bb77afcaa4a7f9e414cbae47da44641
_Platu_ <adamek.trcka@seznam.cz>
2023-03-17 10:24:34 +0100

    oprava download

--- download1.bat
+++ download1.bat
@@ -1,5 +1,5 @@
 @REM Ukázka stažení repozitáře z GitHubu
 
-"C:\Program Files\Git\bin\bash.exe" -login -i -c "git clone https://github.com/Platu123/test_git"
+"C:\Program Files\Git\bin\bash.exe" -login -i -c "git clone https://github.com/Platu123/Trčka1"
 cd test_git
 code .
